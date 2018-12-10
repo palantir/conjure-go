2fa775ac8bae43fa80d789ec8e77f6dd46bff3d1
Nick Miyake <nmiyake@palantir.com>
2018-12-10 11:33:48 -0800

    Update generation code
    * Include license output in conjure-go-verifier generation
    * Disable all godel checks for integration_test/testgenerated

--- conjure-go-verifier/generate.go
+++ conjure-go-verifier/generate.go
@@ -33,7 +33,21 @@ const conjureVerifierVersion = "0.8.0"
 
 func main() {
 	const versionFilePath = "version_test.go"
-	newVersionFileContent := fmt.Sprintf(`// This is a generated file: do not edit by hand.
+	newVersionFileContent := fmt.Sprintf(`// Copyright (c) 2018 Palantir Technologies. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This is a generated file: do not edit by hand.
 // To update this file, run the generator in conjure-go-verifier.
 package verifier_test
 
