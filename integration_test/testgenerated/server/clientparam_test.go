500f011eab8fc6a1a2d2bd9f5f824148bd59a3b4
michaelchen01 <michaelchen92067@gmail.com>
2019-04-30 08:51:46 -0700

    Fix string reference generation in server generation (#50)
    * try again with fixing string reference generation in server generation
    
    * add all files

--- integration_test/testgenerated/server/clientparam_test.go
+++ integration_test/testgenerated/server/clientparam_test.go
@@ -88,6 +88,32 @@ func TestPathParam(t *testing.T) {
 	assert.True(t, called)
 }
 
+func TestPathParamAlias(t *testing.T) {
+	const (
+		wantParam        = "var/conf/install.yml"
+		wantEncodedParam = "var%2Fconf%2Finstall.yml"
+	)
+
+	called := false
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		called = true
+		paramVal := strings.TrimPrefix(req.RequestURI, "/path/alias/")
+		assert.Equal(t, wantEncodedParam, paramVal)
+
+		unescaped, err := url.PathUnescape(paramVal)
+		require.NoError(t, err)
+		assert.Equal(t, wantParam, unescaped)
+
+		rw.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	client := api.NewTestServiceClient(newHTTPClient(t, server.URL))
+	err := client.PathParamAlias(context.Background(), wantParam)
+	require.NoError(t, err)
+	assert.True(t, called)
+}
+
 func TestBytes(t *testing.T) {
 	called := false
 	bytes := make([]byte, 10)
