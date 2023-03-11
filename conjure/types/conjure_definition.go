5ee41e0fbe63827b57f9f02b044e0d99b69ba3b1
Lucas França de Oliveira <lucasfdo@palantir.com>
2023-03-10 17:16:39 -0800

    rem 1: wire everything up

--- conjure/types/conjure_definition.go
+++ conjure/types/conjure_definition.go
@@ -24,9 +24,7 @@ import (
 
 	"github.com/dave/jennifer/jen"
 	"github.com/palantir/conjure-go/v6/conjure-api/conjure/spec"
-	"github.com/palantir/conjure-go/v6/conjure/cycles"
 	"github.com/palantir/conjure-go/v6/conjure/transforms"
-	werror "github.com/palantir/witchcraft-go-error"
 	"github.com/pkg/errors"
 )
 
@@ -51,11 +49,6 @@ type ConjurePackage struct {
 }
 
 func NewConjureDefinition(outputBaseDir string, def spec.ConjureDefinition) (*ConjureDefinition, error) {
-	def, err := cycles.RemovePackageCycles(def)
-	if err != nil {
-		return nil, werror.Wrap(err, "failed to remove package cycles")
-	}
-
 	paths, err := newPathTranslator(outputBaseDir)
 	if err != nil {
 		return nil, err
