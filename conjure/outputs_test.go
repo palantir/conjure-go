9f70ece3585a551853cd7723c4542b17a388f2b6
Avi Radinsky <aradinsky@palantir.com>
2025-11-24 17:04:04 -0500

    test that demonstrates the problem

---
+++ conjure/outputs_test.go
@@ -0,0 +1,39 @@
+// Copyright (c) 2024 Palantir Technologies. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package conjure
+
+import (
+	"testing"
+
+	"github.com/dave/jennifer/jen"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TestOutputFileMultipleRenders verifies that calling Render() multiple times
+// does not change the response
+func TestOutputFileMultipleRenders(t *testing.T) {
+	jenFile := jen.NewFile("testpkg")
+
+	outputFile := newGoFile("/tmp/test.go", jenFile)
+
+	firstRender, err := outputFile.Render()
+	require.NoError(t, err)
+
+	secondRender, err := outputFile.Render()
+	require.NoError(t, err)
+
+	assert.Equal(t, string(firstRender), string(secondRender))
+}
